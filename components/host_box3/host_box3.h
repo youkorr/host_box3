8d29046c3dd0d07830e8bd6a0228543dda413500
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-02 03:38:04 +0200

    Create host_box3.h

---
+++ components/host_box3/host_box3.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/media_player/media_player.h"
+
+// Inclusions nécessaires pour le USB Audio Class Host
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "usb/usb_host.h"
+#include "usb/uac_host.h"
+
+namespace esphome {
+namespace host_box3 {
+
+class HostBox3Component : public Component {
+ public:
+  HostBox3Component();
+  
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::LATE; }
+  
+  // Configuration du périphérique audio USB
+  void init_usb_audio();
+  bool route_audio_to_usb();
+  
+ private:
+  // Variables pour gérer l'audio USB
+  usb_host_client_handle_t client_hdl;
+  uac_host_handle_t uac_handle;
+  bool usb_audio_initialized = false;
+  
+  // Tâche pour gérer les événements USB
+  static void usb_event_task(void *arg);
+  TaskHandle_t usb_task_handle;
+};
+
+}  // namespace host_box3
+}  // namespace esphome

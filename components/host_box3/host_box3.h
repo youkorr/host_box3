cee1d3dc14c8d629824f7d072644e8bac7a9bd19
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-02 06:02:50 +0200

    Update host_box3.h

--- components/host_box3/host_box3.h
+++ components/host_box3/host_box3.h
@@ -2,6 +2,9 @@
 
 #include "esphome/core/component.h"
 #include "usb/usb_host.h"
+#include "esp_usb.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 namespace esphome {
 namespace host_box3 {
@@ -9,22 +12,22 @@ namespace host_box3 {
 class HostBox3Component : public Component {
  public:
   HostBox3Component();
-  
+  ~HostBox3Component();
+
   void setup() override;
   void loop() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::LATE; }
-  
-  // Initialisation du périphérique USB
-  void init_usb_host();
-  
- private:
-  usb_host_client_handle_t client_hdl;
-  usb_phy_handle_t phy_hdl;
-  bool usb_host_initialized = false;
-  TaskHandle_t usb_task_handle;
 
-  // Gestion des événements USB
+ private:
+  usb_host_client_handle_t client_hdl = nullptr;
+  usb_phy_handle_t phy_hdl = nullptr;
+  bool usb_audio_initialized = false;
+  TaskHandle_t usb_task_handle = nullptr;
+
+  void init_usb_audio();
+  bool route_audio_to_usb();
+
   static void usb_event_task(void *arg);
 };
 

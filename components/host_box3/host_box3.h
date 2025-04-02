bc4051c6a22ed23d344585fe80b9ef6638acb4f7
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-02 05:56:10 +0200

    Update host_box3.h

--- components/host_box3/host_box3.h
+++ components/host_box3/host_box3.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/media_player/media_player.h"
 #include "usb/usb_host.h"
-
 
 namespace esphome {
 namespace host_box3 {
@@ -17,19 +15,21 @@ class HostBox3Component : public Component {
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::LATE; }
   
-  void init_usb_audio();
-  bool route_audio_to_usb();
+  // Initialisation du périphérique USB
+  void init_usb_host();
   
  private:
   usb_host_client_handle_t client_hdl;
   usb_phy_handle_t phy_hdl;
-  bool usb_audio_initialized = false;
+  bool usb_host_initialized = false;
   TaskHandle_t usb_task_handle;
 
+  // Gestion des événements USB
   static void usb_event_task(void *arg);
 };
 
 }  // namespace host_box3
 }  // namespace esphome
+
 
 

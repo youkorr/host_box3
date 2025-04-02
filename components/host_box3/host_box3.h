0ad94903c29f835d28aa82a448b4efb2524c31a1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-02 17:34:22 +0200

    Update host_box3.h

--- components/host_box3/host_box3.h
+++ components/host_box3/host_box3.h
@@ -2,33 +2,55 @@
 
 #include "esphome/core/component.h"
 #include "usb/usb_host.h"
-#include "esp_private/usb_phy.h"  // Pour usb_phy_config_t
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
 namespace esphome {
 namespace host_box3 {
 
+// Structure d'événement USB
+typedef struct {
+    int event_id;
+    void *data;
+} usb_audio_event_t;
+
 class HostBox3Component : public Component {
- public:
-  HostBox3Component();
-  ~HostBox3Component();
+public:
+    HostBox3Component();
+    ~HostBox3Component();
 
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
-  float get_setup_priority() const override { return setup_priority::LATE; }
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
+    
+    // Définir la priorité du composant
+    float get_setup_priority() const override { return setup_priority::LATE; }
 
- private:
-  usb_host_client_handle_t client_hdl = nullptr;
-  usb_phy_handle_t phy_hdl = nullptr;
-  bool usb_audio_initialized = false;
-  TaskHandle_t usb_task_handle = nullptr;
-
-  void init_usb_audio();
-  bool route_audio_to_usb();
-
-  static void usb_event_task(void *arg);
+private:
+    // Initialisation de l'hôte USB audio
+    void init_usb_audio();
+    
+    // Configuration du routage audio vers le périphérique USB
+    bool route_audio_to_usb();
+    
+    // Callback pour les événements client USB
+    static void client_event_callback(const usb_host_client_event_msg_t *event_msg, void *arg);
+    
+    // Traitement des événements USB
+    void process_usb_event(usb_audio_event_t *event);
+    
+    // Gestion de la connexion/déconnexion des périphériques
+    void process_device_connection(uint8_t dev_addr);
+    void process_device_disconnection();
+    
+    // Tâche d'événement USB
+    static void usb_event_task(void *arg);
+    
+    // Variables membres
+    usb_host_client_handle_t client_hdl;
+    bool usb_audio_initialized;
+    TaskHandle_t usb_task_handle;
 };
 
 }  // namespace host_box3
